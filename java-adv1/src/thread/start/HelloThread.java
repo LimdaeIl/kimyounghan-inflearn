d0c3785f917fc53e0fb614a4ea80aba489990c11
LimDae94 <piay801@naver.com>
2025-03-05 15:39:44 +0900

    섹션 3. 스레드 생성과 실행: 스레드 시작1

--- java-adv1/src/thread/start/HelloThread.java
+++ java-adv1/src/thread/start/HelloThread.java
@@ -2,8 +2,9 @@ package thread.start;
 
 public class HelloThread extends Thread {
 
-    @Override
-    public void run() {
-        System.out.println(Thread.currentThread().getName() + ": run()");
-    }
+  @Override
+  public void run() {
+    System.out.println(Thread.currentThread().getName() + ": run() ");
+  }
+
 }
--- java-adv1/src/thread/start/HelloThreadMain.java
+++ java-adv1/src/thread/start/HelloThreadMain.java
@@ -1,15 +1,15 @@
 package thread.start;
 
 public class HelloThreadMain {
+  public static void main(String[] args) {
+    System.out.println(Thread.currentThread().getName() + ": main() start"); // main: start()
 
-    public static void main(String[] args) {
-        System.out.println(Thread.currentThread().getName() + ": main() start");
+    HelloThread thread = new HelloThread();
+    System.out.println(Thread.currentThread().getName() + ": start() 호출 전");
+    thread.start(); // run() 이 아니라 start()를 호출해야 한다!
+    System.out.println(Thread.currentThread().getName() + ": start() 호출 후");
 
-        HelloThread helloThread = new HelloThread();
-        System.out.println(Thread.currentThread().getName() + ": start() 호출 전");
-        helloThread.start();
-        System.out.println(Thread.currentThread().getName() + ": start() 호출 후");
+    System.out.println(Thread.currentThread().getName() + ": main() end"); // main: end()
 
-        System.out.println(Thread.currentThread().getName() + ": main() end");
-    }
+  }
 }
